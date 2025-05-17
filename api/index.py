de0da954444a5f06d30527bd4f644c01bb34f761
ftmbotzx <kumarasta91@gmail.com>
2025-05-17 05:44:54 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -21,6 +21,7 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
      'csrfToken': 'zAVdnQAVegC92-ah6pmLf6Dl',
  }
 
+
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:121.0) Gecko/20100101 Firefox/121.0',
     'Accept': '*/*',
