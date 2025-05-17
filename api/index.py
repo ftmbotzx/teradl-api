57660d7ea25d00a9f75722151f45befc4c94c56c
ftmbotzx <kumarasta91@gmail.com>
2025-05-17 05:39:54 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -19,7 +19,6 @@ app = Flask(__name__)
      'csrfToken': 'zAVdnQAVegC92-ah6pmLf6Dl',
  }
 
-
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:121.0) Gecko/20100101 Firefox/121.0',
     'Accept': '*/*',
