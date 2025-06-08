7f26bfe1a42d0d817d8dd9fbc26a4928b6416253
ftmbotzx <kumarasta91@gmail.com>
2025-06-08 12:16:00 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -10,15 +10,15 @@ app = Flask(__name__)
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-cookies = {
+cookies = {Add commentMore actions
     'PANWEB': '1',
-    'browserid': 'p4nVrnlkUVKcnbbJHnIClAhSL5uXs01e-0svx0bm7KHLUB6wIVvCUNGLIpU=',
+    'browserid': '45Cbkepkx0J0bqgQi1e1ubtbstmebahkYOYm3ZWuIktZFHaUuRjvdeeHz24=',
     'lang': 'en',
-    '__bid_n': '1900b9f02442253dfe4207',
-    'ndut_fmt': '5E7E5AFA065E159EF56CFE164FCF084C72B603BE3611911C28550443BDC08A4B',
-    '__stripe_mid': 'b85d61d2-4812-4eeb-8e41-b1efb3fa2a002a54d5',
-    'ndus': 'YylKpiCteHuiYEqq8n75Tb-JhCqmg0g4YMH03MYD',
-    'csrfToken': 'zAVdnQAVegC92-ah6pmLf6Dl',
+    '__bid_n': '196b885ed7339790814207',
+    'ndut_fmt': '26BE3C564003BEB97F4ED69DEBD9974F6291C708F2B2B419F0B6282675131E1C',
+    '__stripe_mid': '31fc92f3-a12a-480f-9e44-53f30f08258a75588e',
+    'ndus': 'YdZTyX1peHuimlux_D6dLGQBeHmj0r3M3trkunHB',
+    'csrfToken': 'C84Gc54sleTMoZxBx24k4SM7',
 }
 
 headers = {
